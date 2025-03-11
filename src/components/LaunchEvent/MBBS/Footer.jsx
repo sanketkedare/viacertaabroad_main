d948b169e9426bf6e199116f92d48794363dd4a0
Sanket Kedare <sanketkedare200@gmail.com>
2025-03-11 09:58:20 +0530

    contact support updated

--- src/components/LaunchEvent/MBBS/Footer.jsx
+++ src/components/LaunchEvent/MBBS/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="flex flex-col items-center justify-center gap-5 w-10/12 m-auto">
         <Image src="/viaCerta-logo.png" alt="ViaCerta Logo" width={150} height={50} className="bg-white rounded-full" />
         <div className="text-center ">
-          📞 Need Help? Contact us at <a href="mailto:support@viacerta.com" className="underline">support@viacerta.com</a> or call +91-XXXXXXXXXX.
+          📞 Need Help? Contact us at <a href="mailto:support@viacerta.com" className="underline">Info@viacertaabroad.com</a> or call +91 83768 95692.
         </div>
         <div className="text-center font-bold">
           🚀 Don’t Miss Out! Limited Slots Available
