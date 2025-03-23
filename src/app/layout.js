d3aca9d75923f6a1d2a3f77d7fa8e1950d436663
Sanket Kedare <sanketkedare200@gmail.com>
2025-03-23 23:15:40 +0530

    Added google analatics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import "./globals.css";
 
 export const metadata = {
@@ -6,10 +7,34 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     window.dataLayer = window.dataLayer || [];
+  //     function gtag() {
+  //       window.dataLayer.push(arguments);
+  //     }
+  //     gtag('js', new Date());
+  //     gtag('config', 'G-GVW27HMVLB');
+  //   }
+  // }, []);
+
   return (
     <html lang="en" data-lt-installed="true">
       <head>
         <link rel="icon" href="/viacertaLogo-white.png" />
+        {/* Google Analytics Script */}
+        <Script 
+          strategy="afterInteractive" 
+          src="https://www.googletagmanager.com/gtag/js?id=G-GVW27HMVLB" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GVW27HMVLB');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
         {children}
