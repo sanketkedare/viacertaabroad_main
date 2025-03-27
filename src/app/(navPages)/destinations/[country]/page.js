12663aaca04e9812c946613d3c45a38e7f6280c9
Sanket Kedare <sanketkedare200@gmail.com>
2025-03-27 19:14:34 +0530

    Pop up form addedd

--- src/app/(navPages)/destinations/[country]/page.js
+++ src/app/(navPages)/destinations/[country]/page.js
@@ -11,36 +11,57 @@ import Testimonials from "@/components/Slider/Testimonials";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
+import { X } from "lucide-react";
+import ServicesForm from "@/components/Services/ServicesForm";
 
 const page = () => {
   const [countryData, setContryData] = useState(null);
   const { country } = useParams();
   const countryName = country.split("-").join(" ");
+  const [formOpen, setFormOpen] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setFormOpen(true);
+    }, 10000);
+  }, []);
 
   useEffect(() => {
     if (countryName) {
       const data = destinations.find((i) => i.name === countryName);
-      console.log(data)
+      console.log(data);
       setContryData(data);
     }
   }, [countryName]);
 
   return (
     <Provider store={store}>
-      <Navbar />
-      <div>
-      {countryData ? (
-        <IndiviualCountry country={countryData} />
-      ) : (
-        <div className="min-h-screen w-full flex justify-center items-center">
-          <p>Please Wait...</p>
+      {formOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50 lg:backdrop-blur-sm">
+          <div className="w-[500px]">
+            <X
+              size={30}
+              onClick={() => setFormOpen(false)}
+              className="bg-white border rounded-full m-auto"
+            />
+
+            <ServicesForm />
+          </div>
         </div>
       )}
+      <Navbar />
+      <div>
+        {countryData ? (
+          <IndiviualCountry country={countryData} />
+        ) : (
+          <div className="min-h-screen w-full flex justify-center items-center">
+            <p>Please Wait...</p>
+          </div>
+        )}
       </div>
-     
-      <Testimonials/>
-      <FAQs/>
-      <Frame4/>
+
+      <Testimonials />
+      <FAQs />
+      <Frame4 />
       <Footer />
     </Provider>
   );
--- src/app/(navPages)/test-prep/[exam]/page.js
+++ src/app/(navPages)/test-prep/[exam]/page.js
@@ -2,21 +2,32 @@
 import FAQs from "@/components/About/FAQs";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import ServicesForm from "@/components/Services/ServicesForm";
 import Testimonials from "@/components/Slider/Testimonials";
 import ExamCart from "@/components/TestPrep/ExamCart";
 import { test_prep } from "@/components/TestPrep/utils";
 import store from "@/Redux/appStore";
+import { X } from "lucide-react";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 
 const page = () => {
+  const [formOpen, setFormOpen] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setFormOpen(true);
+    }, 10000);
+  }, []);
+
   const [error, setError] = useState(false);
   const [examData, setExamData] = useState(null);
   const { exam } = useParams();
 
   const getData = () => {
-    const data = test_prep.find((i) => i.name.toLowerCase() === exam.toLowerCase() );
+    const data = test_prep.find(
+      (i) => i.name.toLowerCase() === exam.toLowerCase()
+    );
     return data;
   };
 
@@ -32,6 +43,20 @@ const page = () => {
   }, [exam]);
   return (
     <Provider store={store}>
+      {formOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50 lg:backdrop-blur-sm">
+          <div className="w-[500px]">
+            <X
+              size={30}
+              onClick={() => setFormOpen(false)}
+              className="bg-white border rounded-full m-auto"
+            />
+
+            <ServicesForm />
+          </div>
+        </div>
+      )}
+
       <Navbar />
       {error ? (
         <div className="flex justify-center items-center w-full h-[50vh] text-xl font-bold">
