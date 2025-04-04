76b7b7a579cf0433ec1a222bf979050c84d0f27f
Sanket Kedare <sanketkedare200@gmail.com>
2025-04-04 17:07:26 +0530

    .

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,36 +1,42 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
+const bulletPointSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  info: { type: String, required: true },
+}, { _id: false });
 
-    author: {
-      name: {
-        type: String,
-      },
-      image: {
-        type: String,
-      },
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-    },
+const subtopicSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  intro: { type: String },
+  info: { type: String },
+  bulletpoints: [bulletPointSchema],
+  ending: { type: String },
+}, { _id: false });
 
-    intro: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const tableSchema = new mongoose.Schema({
+  headings: [{ type: String }],
+  fields: [{ type: String }],
+}, { _id: false });
+
+const authorSchema = new mongoose.Schema({
+  name: { type: String },
+  image: { type: String },
+}, { _id: false });
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  intro: { type: String },
+  subtopics: [subtopicSchema],
+  country: { type: String },
+  date: { type: String },
+  thumbnail: { type: String },
+  meta: { type: String },
+  table: tableSchema,
+  author: authorSchema,
+  image: { type: String, required: true },
+}, {
+  timestamps: true,
+});
 
 const Blog = mongoose.models.blogs || mongoose.model("blogs", blogSchema);
 
