f4c63c9e3725f10e5ac60c19b4edb51964f85d58
Sanket Kedare <sanketkedare200@gmail.com>
2025-04-07 13:04:29 +0530

    New Blog Addedd

---
+++ src/components/MBBS/EligibilityCriteria.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EligibilityCriteria = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EligibilityCriteria
--- src/components/MBBS/MBBS.jsx
+++ src/components/MBBS/MBBS.jsx
@@ -8,8 +8,9 @@ import HeroMBBS from "./HeroMBBS";
 import IntroMBBS from "./IntroMBBS";
 import KeyBenifits from "./KeyBenifits";
 import FAQs from "../About/FAQs";
-import faq from "./mbbsFaq.json"
+import faq from "./mbbsFaq.json";
 import BlogSlider from "../Slider/BlogSlider";
+import EligibilityCriteria from "./EligibilityCriteria";
 
 const MBBS = () => {
   const commonData = {
@@ -24,32 +25,37 @@ const MBBS = () => {
       },
       button: "Start Your MBBS journey ",
     },
-    intro:{
-        heading :"Why Study MBBS Abroad?",
-        content:"Studying MBBS abroad is an excellent choice for students who want a high-quality medical education at an affordable cost. Many countries offer medical programs that are globally recognized, ensuring that students have access to international career opportunities.\nAt ViaCerta Abroad, we simplify the entire admission process, making it easier for you to secure a seat in top medical universities without unnecessary stress."
+    intro: {
+      heading: "Why Study MBBS Abroad?",
+      content:
+        "Studying MBBS abroad is an excellent choice for students who want a high-quality medical education at an affordable cost. Many countries offer medical programs that are globally recognized, ensuring that students have access to international career opportunities.\nAt ViaCerta Abroad, we simplify the entire admission process, making it easier for you to secure a seat in top medical universities without unnecessary stress.",
     },
-    benifits:{
-        heading:"Key Benefits of Studying MBBS Abroad",
-        items: [
-            "No Entrance Exams - Many universities accept direct admissions.",
-            "Affordable Tuition Fees - Get a world-class education at a fraction of the cost.",
-            "Globally Recognized Degree - Accredited by WHO, MCI, ECFMG, and more.",
-            "Modern Medical Infrastructure - Advanced labs, research facilities, and practical training.",
-            "International Exposure - Diverse student community and global career prospects.",
-            "Modern Facilities & Research Opportunities - Advanced laboratories, hospital training, and innovative learning environments."
-          ]
-    }
+    benifits: {
+      heading: "Key Benefits of Studying MBBS Abroad",
+      items: [
+        "No Entrance Exams - Many universities accept direct admissions.",
+        "Affordable Tuition Fees - Get a world-class education at a fraction of the cost.",
+        "Globally Recognized Degree - Accredited by WHO, MCI, ECFMG, and more.",
+        "Modern Medical Infrastructure - Advanced labs, research facilities, and practical training.",
+        "International Exposure - Diverse student community and global career prospects.",
+        "Modern Facilities & Research Opportunities - Advanced laboratories, hospital training, and innovative learning environments.",
+      ],
+    },
   };
   return (
     <Provider store={store}>
       <Navbar />
       <HeroMBBS obj={commonData.heroSection} />
       <IntroMBBS obj={commonData.intro} />
-      <KeyBenifits obj= {commonData.benifits}/>
-      <FAQs faqArray={faq}/>
-      <BlogSlider/>
+      <KeyBenifits obj={commonData.benifits} />
 
-     
+      {/* -------- Destinations Here -------------- */}
+
+      {/* Eligibility Criteria */}
+      <EligibilityCriteria/>
+      
+      <FAQs faqArray={faq} />
+      <BlogSlider />
       <Footer />
     </Provider>
   );
